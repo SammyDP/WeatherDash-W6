de3146ffb5991b6e1edfc9d81bc346361578de62
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-08-02 11:29:20 -0600

    Update script.js

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -2,6 +2,13 @@ var searchBtn = document.querySelector("#search_btn");
 var inputEl = document.querySelector(".input");
 
 searchBtn.addEventListener("click", function () {
-  console.log("hello world");
-  console.log(inputEl.value);
+  var city = inputEl.value;
+  //   fetch request to api using city
+  // use geocode api with city to get lat/long
+  // call getWeather(data[0].lat, data[0].lon)
 });
+
+// create getWeather(lat, lon)
+// fetch request to weather api
+// create weather cards (5)
+// for loop to run through the data
